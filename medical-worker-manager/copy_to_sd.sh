2d51d466775ca4eee1c6adfb6449f5403cfa17ff
iillaa <36302274+iillaa@users.noreply.github.com>
2025-12-13 03:32:14 +0100

    Delete medical-worker-manager directory

--- medical-worker-manager/copy_to_sd.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-mkdir -p /storage/emulated/0/medical-worker-manager-build
-cp -r dist /storage/emulated/0/medical-worker-manager-build/
-echo "Build copied to /storage/emulated/0/medical-worker-manager-build/"
